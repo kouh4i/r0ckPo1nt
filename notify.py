9f6f102f726fff360fdd1c7d221aa4ca92d99d83
LiLi <137469486+lilith1306@users.noreply.github.com>
2024-05-13 22:18:46 -0300

    Update notify.py

--- notify.py
+++ notify.py
@@ -6,11 +6,11 @@ url = sys.argv[1]
 
 ctypes.windll.kernel32.FreeConsole()
 
-teste = notification.notify(
+ntf = notification.notify(
     title='CUIDADO!',
     message=f'A URL {url} foi encontrada na blacklist.',
     app_icon=r'C:\Users\User\Downloads\spy.ico',
     timeout=7  # Duração da notificação em segundos
     )
-print(teste)
+print(ntf)
 
